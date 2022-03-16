38bd7b6b6384d3c18c1b96bb0efe95c4db10ecb1
valiant1412 <nguyenminhduc2c19@gmail.com>
2022-03-16 23:17:13 +0700

    learning about scss

--- src/server.js
+++ src/server.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'))
 app.engine('hbs', handlebars.engine({
     extname: ".hbs"
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'resources\\views'));
-console.log("path", path.join(__dirname, "resources/views"))
 app.get('/', (req, res) => {
     res.render('home');
 })
